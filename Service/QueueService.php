fbf2ac3738265f8b522e3b82baea6a2360fee698
Alexandre Mogère <heristop@yahoo.fr>
2016-02-23 13:28:11 +0100

    [Project] Added SF3 support

--- Service/QueueService.php
+++ Service/QueueService.php
@@ -12,12 +12,12 @@
 namespace Heri\Bundle\JobQueueBundle\Service;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\HttpKernel\Log\LoggerInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\StreamOutput;
 use Symfony\Component\Process\Process;
+use Psr\Log\LoggerInterface;
 use Heri\Bundle\JobQueueBundle\Exception\InvalidUnserializedMessageException;
 
 class QueueService
