dcf8c2b2a053a8654567645562249637ec0ca075
Alexandre Mogère <heristop@yahoo.fr>
2016-09-07 11:22:00 +0200

    Added LICENSE

--- Exception/UnsupportedMethodCallException.php
+++ Exception/UnsupportedMethodCallException.php
@@ -1,7 +1,9 @@
 <?php
 
-/**
+/*
  * This file is part of HeriJobQueueBundle.
+ *
+ * (c) Alexandre Mogère
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
